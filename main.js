07402d7d9f8e9498a333e74ca5745940bf93f933
Lassieena <lassieena@gmail.com>
2019-06-29 00:34:03 +0900

    確率操作

--- main.js
+++ main.js
@@ -33,8 +33,28 @@ function removeAllChildren(element) {
 
 function divination() {
   const horos = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶'];
-  const num = Math.floor(Math.random() * horos.length);
-  fortune.innerText = horos[num];
+  const num = Math.random();
+  if (num < 0.01) {
+    fortune.innerText = horos[0];
+  } else if (num < 0.11) {
+    fortune.innerText = horos[1];
+
+  } else if (num < 0.25) {
+    fortune.innerText = horos[2];
+
+  } else if (num < 0.38) {
+    fortune.innerText = horos[3];
+
+  } else if (num < 0.52) {
+    fortune.innerText = horos[4];
+
+  } else if (num < 0.75) {
+    fortune.innerText = horos[5];
+
+  } else if (num < 1) {
+    fortune.innerText = horos[6];
+
+  }
 
 
   const miliG = ['カフェイン', '塩分', '糖質', '脂質', 'たんぱく質', 'ミネラル', 'ビタミン', '食物繊維', '脂質'];
@@ -52,7 +72,8 @@ function divination() {
     'あなたの挑戦を祈っております。',
     'これができれば君も竜王だ!!',
     'タピオカも一緒に食べよう!',
-    'ラーメン食べれば解決だね'
+    'ラーメン食べれば解決だね',
+    'モンスターエナジー飲めば解決だね。'
   ];
   const happ = Math.floor(Math.random() * happiness.length);
 
